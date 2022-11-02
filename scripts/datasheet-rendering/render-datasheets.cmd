617a23e57711a9a258d0c99283b915a4c20e4e16
marqdevx <p.marquinez@arduino.cc>
2022-11-03 00:39:46 +0100

    Install fonts on windows

--- scripts/datasheet-rendering/render-datasheets.cmd
+++ scripts/datasheet-rendering/render-datasheets.cmd
@@ -1,16 +1,20 @@
 @ECHO OFF
 
+::Install fonts
+copy styles\fonts %localappdata%\Microsoft\Windows\Fonts /Y
+echo "Font installed on the system:"
+dir %localappdata%\Microsoft\Windows\Fonts
+
 node -v
 IF %ERRORLEVEL% NEQ 0 (
     cls
     ECHO Please install Node.js from here https://nodejs.org/en/download/
     EXIT /B
 ) else (
-    cls
 
     ::Only install the modules if npm list return an Error
     npm list --depth=0 || npm install && cls && ECHO Modules installed
 
-    :: argument %* only used with "Current directory" option
+    ::Argument %* only used with "Current directory" option
     npx datasheet-renderer config.json %*
 )
