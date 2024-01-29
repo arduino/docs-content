30f657257bb5ba0deb245110c7764411f88d287f
Matteo Marsala <m.marsala@arduino.cc>
2024-01-29 08:28:09 +0100

    Update scripts/datasheet-rendering/render-datasheets.sh
    Co-authored-by: Sebastian Romero <s.romero@arduino.cc>

--- scripts/datasheet-rendering/render-datasheets.sh
+++ scripts/datasheet-rendering/render-datasheets.sh
@@ -29,7 +29,6 @@ if [ -n "$CI" ]; then
     fc-list | grep "NotoSansSC"
     fc-list | grep "NotoSansTC"
 
-    # apt-get install fonts-wqy-microhei ttf-wqy-microhei fonts-wqy-zenhei ttf-wqy-zenhei
     apt-get -qq update -y
     # SEE: https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#chrome-headless-doesnt-launch-on-unix
     apt-get -qq install -y -o=Dpkg::Use-Pty=0 ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
