f0d1917f46c036d7df6c9df49820e1f8d85db360
Matteo Marsala <matteo.marsala92@gmail.com>
2025-05-16 11:05:06 +0200

    update registry

--- scripts/datasheet-rendering/render-datasheets.sh
+++ scripts/datasheet-rendering/render-datasheets.sh
@@ -28,8 +28,13 @@ if [ -n "$CI" ]; then
     sudo apt-get -qq install -y -o=Dpkg::Use-Pty=0 ca-certificates fonts-liberation libasound2 libappindicator3-1 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
 
     echo "Running on Node version: `node -v`"
+
+    echo "Setting up registry..."
+    npm config set registry https://registry.npmjs.org/
+    echo "Registry attuale: $(npm config get registry)"
+
     echo "Installing dependencies..."
-    npm install --verbose
+    export PUPPETEER_SKIP_DOWNLOAD=true npm install --verbose
 
     echo "Running datasheet-renderer..."
     time npx datasheet-renderer config.json
